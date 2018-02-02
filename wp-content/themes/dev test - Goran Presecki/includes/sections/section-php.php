e57d2ae8a65a957cf0e635bc2d6d9f17544b61f6
Goran Presečki <goran.presecki@gmail.com>
2018-02-02 20:43:56 +0100

    PHP test - Part two: ACF fields connected to front-page

--- wp-content/themes/dev test - Goran Presecki/includes/sections/section-php.php
+++ wp-content/themes/dev test - Goran Presecki/includes/sections/section-php.php
@@ -13,11 +13,11 @@
       <li><strong>Part three (bonus round):</strong> when you submit the form, it should open the confirmation modal at the center of the page that lists all the options picked by the user and has a couple of buttons, "OK" and "Cancel". "Cancel" will close the modal, and "OK" will send you to the front page of the test along with the data.</li>
     </ol>
     <form>
-      <label for="checkbox_1">I'd like to check this box</label><input type="checkbox" name="checkbox" id="checkbox_1" value="value">
-      <label for="checkbox_2">I don't like this other box</label><input type="checkbox" name="checkbox" id="checkbox_2" value="value">
-      <label for="checkbox_3">I don't care about this box</label><input type="checkbox" name="checkbox" id="checkbox_3" value="value">
-      <label for="checkbox_4">I don't care about this box, either</label><input type="checkbox" name="checkbox" id="checkbox_4" value="value">
-      <input type="submit">
+      <label for="checkbox_1"><?php $tmp = get_field( 'checkbox_1_label' ); echo $tmp ? $tmp : "I'd like to check this box"; ?></label><input type="checkbox" name="checkbox_1" id="checkbox_1" value="1"<?php echo get_field( 'checkbox_1_state' ) ? ' checked' : ''; ?>>
+      <label for="checkbox_2"><?php $tmp = get_field( 'checkbox_2_label' ); echo $tmp ? $tmp : "I don't like this other box"; ?></label><input type="checkbox" name="checkbox" id="checkbox_2" value="1"<?php echo get_field( 'checkbox_2_state' ) ? ' checked' : ''; ?>>
+      <label for="checkbox_3"><?php $tmp = get_field( 'checkbox_3_label' ); echo $tmp ? $tmp : "I don't care about this box"; ?></label><input type="checkbox" name="checkbox" id="checkbox_3" value="1"<?php echo get_field( 'checkbox_3_state' ) ? ' checked' : ''; ?>>
+      <label for="checkbox_4"><?php $tmp = get_field( 'checkbox_4_label' ); echo $tmp ? $tmp : "I don't care about this box, either"; ?></label><input type="checkbox" name="checkbox" id="checkbox_4" value="1"<?php echo get_field( 'checkbox_4_state' ) ? ' checked' : ''; ?>>
+      <input type="submit" value="<?php $tmp = get_field( 'submit_button_text' ); echo $tmp ? $tmp : 'Submit'; ?>">
     </form>
     <div class="section-icon"><?php get_template_part('includes/svg/php'); ?></div>
   </div>
