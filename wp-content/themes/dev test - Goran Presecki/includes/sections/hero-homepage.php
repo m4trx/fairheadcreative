1dd1de227550637c955d2f0616c7c9c9f9227db5
Goran Presečki <goran.presecki@gmail.com>
2018-02-02 17:43:13 +0100

    Js test - jsOne task: Main navigation srolls on click 1.2 seconds to target

--- wp-content/themes/dev test - Goran Presecki/includes/sections/hero-homepage.php
+++ wp-content/themes/dev test - Goran Presecki/includes/sections/hero-homepage.php
@@ -4,9 +4,9 @@
     <p>Access the part you want to work on by clicking the appropriate button</p>
     <p>Don't limit yourself to just one field, show us all that you can do - the more you solve, the better your chances. What this means is if you ace the CSS test and do some of the PHP, you'll stand a better chance than someone who's only aced the CSS. Remember to check out the README.md to make sure you understand what we're looking for.</p>
     <p>
-      <a href="#css" class="button">CSS</a>
-      <a href="#js" class="button">JS</a>
-      <a href="#php" class="button">Backend/PHP</a>
+      <a href="#css" class="button" data-scroll-speed="1200">CSS</a>
+      <a href="#js" class="button" data-scroll-speed="1200">JS</a>
+      <a href="#php" class="button" data-scroll-speed="1200">Backend/PHP</a>
     </p>
     <div class="section-icon"><?php get_template_part('includes/svg/logo-shield'); ?></div>
   </div>
